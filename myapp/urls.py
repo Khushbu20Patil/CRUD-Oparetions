ae3c8129264729a085e7f925e26412a9b348e873
Khushbu Patil <khushbup.patil@thinkitive.com>
2022-08-16 14:51:52 +0530

    code updated

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,22 +1,15 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from myapp.models import UserModel
-
-
 from rest_framework import routers
- 
-# import everything from views
 from .views import *
  
-# define the router
 router = routers.DefaultRouter()
-router.register('productviewset', ProductViewSet)
+router.register('product', ProductViewSet)
 router.register('productmixgen', CreateListRetrieveViewSet)
 router.register('productsimpleviewset', ProductSimpleViewSet)
 
 urlpatterns = [
-
     path('api-auth/', include('rest_framework.urls')),
     path('productcreate/', ProductView, name='product'),
     path('product_detail/<int:pk>/', product_detail, name='product_detail'),
@@ -25,16 +18,11 @@ urlpatterns = [
     path('usergenmix/',UserGenMix.as_view()),
     path('productgenmix/',ProductGenMix.as_view()),
     path('productgenmixcrud/<int:pk>',ProductGenMixDetail.as_view()),
-
     path('productviewset/', include(router.urls)),
-
     path('createlistretrieveviewset/', include(router.urls)),
     path('productsimpleviewset/', include(router.urls)),
-
-  
 ]
 
   
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,106 +1,62 @@
-from itertools import product
-import re
-from django.shortcuts import render
 from rest_framework.response import Response
-# from .models import UserModel, Producdmodel
-from .serializers import ProductModelSerializer , UserModelSerializer
-from .models import UserModel, PoductModel
-from rest_framework.decorators import api_view, action
+from .serializers import ProductModelSerializer
+from .models import PoductModel
+from rest_framework.decorators import api_view
 from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework import mixins,generics,status, viewsets, permissions
-
+from rest_framework import mixins, generics, status, viewsets
 
 # Create your views here.
-@api_view(['GET','POST'])
-def ProductView(request): #product_view(request)
+@api_view(['GET', 'POST'])
+def ProductView(request):
     if request.method == 'POST':
-        # user=UserModel.objects.filter(id=request.data.get('User')).first()
         serializer = ProductModelSerializer(data=request.data)
         if serializer.is_valid():
-            # serializer.user=user
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-
     if request.method == 'GET':
-        # user=UserModel.objects.filter(id=request.data.get('User')).first()
         products = PoductModel.objects.all()
         serializer = ProductModelSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-     
- 
-@api_view(['GET','PUT','PATCH','DELETE'])
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def product_detail(request, pk):
     try:
         product = PoductModel.objects.get(pk=pk)
     except product.DoesNotExist:
         return Response(data={'msg':'Product not exist'},status=status.HTTP_404_NOT_FOUND)
-  
     if request.method == 'GET':
         serializer = ProductModelSerializer(product)
         return Response(serializer.data)
-  
     elif request.method == 'PUT':
         serializer = ProductModelSerializer(product, data=request.data)
-  
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors,
-                        status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'PATCH':
-        serializer = ProductModelSerializer(product,
-                                           data=request.data,
-                                           partial=True)
-  
+        serializer = ProductModelSerializer(product, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
-  
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-        
-
-
-# @api_view(['GET','POST'])
-# def UserView(request):
-#     if request.method == 'POST':
-#         # serializer = UserModelSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=201)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-#     if request.method == 'GET':
-#         return Response({"message": "Hello, world!"})
-#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-        
-########################################################ZZZZZZZZZZZZZZ
-#ZZZZZZZZZ class base view ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-########################################################ZZZZZZZZZZZZZZ
-
 # API View Get & POST
 class ProductList(APIView):
-
-    def get(self, request, format=None):
+    def get(self, request):
         snippets = PoductModel.objects.all()
         serializer = ProductModelSerializer(snippets, many=True)
         return Response(serializer.data)
-
-    def post(self, request, format=None):
+    def post(self, request):
         serializer = ProductModelSerializer(data=request.data)
-        # user=UserModel.objects.filter(id=request.data.get('User')).first()
         serializer = ProductModelSerializer(data=request.data)
         if serializer.is_valid():
-            # serializer.user=user
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -113,12 +69,12 @@ class CBVProductDetail(APIView):
         except PoductModel.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
+    def get(self, pk):
         product = self.get_object(pk)
         serializer = ProductModelSerializer(product)
         return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
+    def put(self, request, pk,):
         snippet = self.get_object(pk)
         serializer = ProductModelSerializer(snippet, data=request.data)
         if serializer.is_valid():
@@ -126,15 +82,11 @@ class CBVProductDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
+    def delete(self, request, pk):
         product = self.get_object(pk)
-        # product = PoductModel.object.get(id=pk)
         product.delete()
         return Response({"message": "product deleted"},status=status.HTTP_204_NO_CONTENT)
 
-
-
-#######################
 # mixins and generics
 class UserGenMix(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
@@ -151,9 +103,6 @@ class ProductGenMix(mixins.ListModelMixin, mixins.CreateModelMixin,generics.Gene
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        # user=UserModel.objects.filter(id=request.data.get('User')).first()
-        serializer = ProductModelSerializer(data=request.data)
-        # serializer.user=user
         return self.create(request, *args, **kwargs)
 
 class ProductGenMixDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
@@ -169,24 +118,14 @@ class ProductGenMixDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixi
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
-
-
-
-
-##############################################
-# from rest_framework import viewsets
-
-# Model Viewset
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = PoductModel.objects.all()
     serializer_class = ProductModelSerializer
 
 #simpleviewset
-class ProductSimpleViewSet(viewsets.ModelViewSet):
+class ProductSimpleViewSet(viewsets.ViewSet):
     queryset = PoductModel.objects.all()
     serializer_class = ProductModelSerializer
-
-
 
 #genric and mixins
 class CreateListRetrieveViewSet(mixins.CreateModelMixin,
